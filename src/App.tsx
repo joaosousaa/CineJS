abf73e8fe9a2068988366d49fd8086841d461377
joaosousaa <joaofranciscosouza04@gmail.com>
2026-01-14 21:03:25 -0300

    favicon e titulo adicionado

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import Header from "./components/header";
-import Footer from "./components/footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 import MoodSelector from "./components/MoodSelector";
 import Results from "./pages/Results";
