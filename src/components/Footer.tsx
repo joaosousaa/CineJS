ea420e879d0c62af83c62cd3963d404c0cbd0bf6
joaosousaa <joaofranciscosouza04@gmail.com>
2026-01-14 21:06:44 -0300

    link github adicionado

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="mt-20 py-6 text-center text-gray-400 border-t border-white/10">
-            <p>© 2025 JSousa – Todos os direitos reservados.</p>
+            <a href="https://github.com/joaosousaa" target="_blank" rel="noopener noreferrer" className="hover:underline">© 2025 JSousa – Todos os direitos reservados.</a>
         </footer>
     );
 }
